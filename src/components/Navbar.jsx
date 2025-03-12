57fa8b1d659be99444a9c53e23bc9229ac860c97
Sandi Sopian <shunasand73@gmail.com>
2025-03-12 14:49:40 +0700

    fix logo and icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const { quoteLogo } = navItems;
   return (
     <nav className="flex fixed top-0 left-0 w-full bg-white/30 backdrop-blur-lg bg-opacity-80 text-white p-4 z-50 gap-2">
-      <img src={quoteLogo} alt="Logo" className="w-5 sm:w-10 rounded-lg" />
+      <img src={quoteLogo} alt="Logo" className="w-7 sm:w-10 rounded-lg" />
       <h1 className="sm:text-2xl font-bold text-gray-700">Quotes Of The Day</h1>
     </nav>
   );
