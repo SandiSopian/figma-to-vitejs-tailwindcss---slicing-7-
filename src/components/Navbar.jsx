cefd4887b37cb6a5c496f90152db8904dd643093
Sandi Sopian <shunasand73@gmail.com>
2025-03-12 14:51:37 +0700

    fix nav logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,11 @@ import { navItems } from "../services/database";
 const Navbar = () => {
   const { quoteLogo } = navItems;
   return (
-    <nav className="flex fixed top-0 left-0 w-full bg-white/30 backdrop-blur-lg bg-opacity-80 text-white p-4 z-50 gap-2">
-      <img src={quoteLogo} alt="Logo" className="w-7 sm:w-10 rounded-lg" />
-      <h1 className="sm:text-2xl font-bold text-gray-700">Quotes Of The Day</h1>
+    <nav className="flex fixed top-0 left-0 w-full bg-white/30 backdrop-blur-lg bg-opacity-80 text-white p-4 z-50">
+      <div className="flex items-center gap-2">
+        <img src={quoteLogo} alt="Logo" className="w-7 sm:w-10 rounded-lg" />
+        <h1 className="sm:text-2xl font-bold text-gray-700">Quotes Of The Day</h1>
+      </div>
     </nav>
   );
 };
